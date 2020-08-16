5417ce1d21bbd1c9f260f537d84869257f9707ee
joryulife <joryulife@gmail.com>
2020-08-16 12:54:22 +0900

    del

--- separate.py
+++ separate.py
@@ -7,24 +7,7 @@ def separate(text):
     todos = todos[1:]
     return todos
 
-def delhash(text,key):
-    text = text.strip(key)
-    return text
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 #テスト
-"""
-text = "・今日のご飯は何かな\n・今日はメモ帳を買うぞ\n・宿題をするぞ"
-print(separate(text))
-"""
-=======
-text = "・今日の昼ご飯を作る\n・今日はメモ帳を買うぞ\n・宿題をするぞ"
-print(separate(text))
-=======
 # text = "・今日の昼ご飯を作る\n・今日はメモ帳を買うぞ\n・宿題をするぞ\n#TODOリストゴーヤ"
 # print(separate(text))
->>>>>>> a08c86374c03b26af0b359ffeb102aeef5752f71
-
->>>>>>> 35c6e3f6bbc3acbfcf4f85fb17f7c7dd097eddfd
